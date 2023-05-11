543a61efb1619eb1dbf18f7fd9f4339a36cfb468
Arem <tgz57031@gmail.com>
2023-05-10 19:24:32 -0500

    ta

--- js/funciones.js
+++ js/funciones.js
@@ -7,10 +7,10 @@
 
 var botonLuz = document.getElementById("switch");
 
-function estado_bombillo(){
-    document.body.style= `background: rgb(2,0,36);
-    background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(227,227,227,1) 61%);`;
-}
+function estado_bombillo() {
+    const body = document.body;
+    body.classList.toggle('bombillo-prendido');
+  }
 
 var luz = document.querySelector(".luz");
 
